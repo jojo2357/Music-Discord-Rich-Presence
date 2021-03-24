f1e101919c3e57855ff85cc5e9e5c35bd67d5db3
ODAKAB <odakab54@gmail.com>
2021-03-24 21:04:54 +0300

    Convert shortcuts into one menu using batch script (#29)
    * Convert shortcuts into one menu using batch script
    
    * Add shortcut generator
    
    * Add generate shortcuts option to main menu
    
    * Revert removal of KillHidden and call it from MusicDRP menu
    
    * Minor changes
    
    Menu doesnt require admin, c# creates shortcuts in order to have the right icons, remove the finder shortcuts because most people only have to run them once anyway and any bat that needs admin will demand it instead of needing shortcuts with admin
    
    * 1.4.4 release
    
    Co-authored-by: jojo2357 <66704796+jojo2357@users.noreply.github.com>

--- GroovyRP/bin/Release/LinkWithSpotify.bat
+++ GroovyRP/bin/Release/LinkWithSpotify.bat
@@ -4,6 +4,9 @@ rem setlocal ENABLEDELAYEDEXPANSION
 set directory=%~dp0
 Pushd %directory%
 
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
 echo Checking admin rights...
 net session>nul
 
@@ -39,9 +42,10 @@ schtasks /create /tn SpotifyRichPresenceClose /XML LinkWithSpotifyClose.xml
 
 pause
 
-goto :eof
+exit
 
 :fnf
 echo Could not locate Spotify.exe
 pause
+exit
 goto :eof
---
+++ Music_DRP_Launcher.bat
@@ -0,0 +1,59 @@
+:::
+:::  /$$      /$$                     /$$           /$$$$$$$  /$$$$$$$  /$$$$$$$ 
+::: | $$$    /$$$                    |__/          | $$__  $$| $$__  $$| $$__  $$
+::: | $$$$  /$$$$ /$$   /$$  /$$$$$$$ /$$  /$$$$$$$| $$  \ $$| $$  \ $$| $$  \ $$
+::: | $$ $$/$$ $$| $$  | $$ /$$_____/| $$ /$$_____/| $$  | $$| $$$$$$$/| $$$$$$$/
+::: | $$  $$$| $$| $$  | $$|  $$$$$$ | $$| $$      | $$  | $$| $$__  $$| $$____/ 
+::: | $$\  $ | $$| $$  | $$ \____  $$| $$| $$      | $$  | $$| $$  \ $$| $$      
+::: | $$ \/  | $$|  $$$$$$/ /$$$$$$$/| $$|  $$$$$$$| $$$$$$$/| $$  | $$| $$      
+::: |__/     |__/ \______/ |_______/ |__/ \_______/|_______/ |__/  |__/|__/      
+:::
+::: [0] Generate Shortcuts
+::: [1] Launch DRP Hidden
+::: [2] Launch DRP
+::: [3] Kill Hidden DRP
+::: [4] Link Rich Presence with Groove
+::: [5] Link with MusicBee
+::: [6] Unlink Rich Presence with Groove
+::: [7] Unlink from MusicBee
+:::
+
+@echo off
+color e
+set mypath=%~dp0
+set mypath=%mypath:~0,-1%
+:MENU
+cls
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+SET /P O=Pick an option: 
+echo ------------------------------------------------------------------------------
+IF %O%==0 GOTO LAUNCHDRPHIDDEN
+IF %O%==1 GOTO LAUNCHDRPHIDDEN
+IF %O%==2 GOTO LAUNCHDRP
+IF %O%==3 GOTO KILLHIDDENDRP
+IF %O%==4 GOTO LINKDRPGROOVE
+IF %O%==5 GOTO LINKDRPMUSICBEE
+IF %O%==6 GOTO UNLINKDRPGROOVE
+IF %O%==7 GOTO UNLINKDRPMUSICBEE
+GOTO MENU
+:LAUNCHDRPHIDDEN
+run "%mypath%\GroovyRP\bin\Release\RunHidden.vbs"
+GOTO MENU
+:LAUNCHDRP
+call "%mypath%\GroovyRP\bin\Release\RunHidden.bat"
+GOTO MENU
+:KILLHIDDENDRP
+call "%mypath%\KillHidden.bat"
+GOTO MENU
+:LINKDRPGROOVE
+call "%mypath%\GroovyRP\bin\Release\LinkWithGroove.bat"
+GOTO MENU
+:LINKDRPMUSICBEE
+call "%mypath%\GroovyRP\bin\Release\LinkWithMusicBee.bat"
+GOTO MENU
+:UNLINKDRPGROOVE
+call "%mypath%\GroovyRP\bin\Release\UnlinkFromGroove.bat"
+GOTO MENU
+:UNLINKDRPMUSICBEE
+call "%mypath%\GroovyRP\bin\Release\UnlinkFromMusicBee.bat"
+GOTO MENU
